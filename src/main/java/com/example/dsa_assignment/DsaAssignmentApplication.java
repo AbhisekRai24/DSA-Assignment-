22f77193c086b73693b6ae344891acd465209621
ahbisek <abhisek1999rai@gmail.com>
2024-08-19 12:24:36 +0545

    question 1 and 2

---
+++ src/main/java/com/example/dsa_assignment/DsaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsa_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsa_assignment/DsaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsa_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
